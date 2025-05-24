14505c20287019ace9e30cd0d26d898939bff446
Nassim Madrane <nassim.madrane@proton.me>
2025-05-24 16:13:02 +0200

    added ws to game for both character switching and login in if not browser

--- Godot/integration-project-gamr/scripts/game_menu.gd
+++ Godot/integration-project-gamr/scripts/game_menu.gd
@@ -1,5 +1,5 @@
 extends Control
-const HOST = "ws://localhost:3000/character/switch"
+const HOST = "wss://itip-team12-backend-itip-project12.apps.okd.ucll.cloud/"
 const DEFAULT_CHARACTER = "res://scenes/Characters/magician.tscn"
 
 var websocket = WebSocketPeer.new()
@@ -19,7 +19,29 @@ func _ready():
 	character_database_reference = preload("res://scripts/CharacterDatabase.gd")
 	$VBoxContainer/preGame.grab_focus() 
 	create_character_node(DEFAULT_CHARACTER)
+	get_username()
 	connect_rfid()
+	
+	$VBoxContainer/Logout.visible = false
+	$VBoxContainer/Logout.disabled = true
+	
+	if OS.has_feature("web"):
+		$VBoxContainer/Login.visible = false
+		$VBoxContainer/Login.disabled = true
+		$VBoxContainer/cardGame.disabled = true
+	else:
+		$"VBoxContainer/preGame".disabled = true
+		$"VBoxContainer/cardGame".disabled = true
+		$VBoxContainer/Scan.disabled = true
+		$VBoxContainer/Scan.visible = false
+
+
+func get_username():
+	var username = JavaScriptBridge.eval("localStorage.getItem('username');")
+	print(username)
+	if username:
+		Global.username = username
+	$Username.text = Global.username
 
 func create_character_node(path):
 	if current_character_scene:
@@ -34,12 +56,14 @@ func create_character_node(path):
 func _on_game_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
+func _on_card_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Card/main.tscn")
 
 func _on_exit_to_login_pressed() -> void:
 	get_tree().quit()
 
 func connect_rfid():
-	var err = websocket.connect_to_url(HOST,null)
+	var err = websocket.connect_to_url(HOST + "character/switch",null)
 	if err != OK:
 		print("Unable to connect to server: ", err)
 		start_reconnection()
@@ -111,9 +135,16 @@ func change_character(cardUid):
 	var character_name = character_database_reference.CHARACTERS.find_key(cardUid)
 	if character_name:
 		create_character_node("res://scenes/Characters/"+ character_name +".tscn")
+		Global.character = character_name
 		$StatusMessage.text = ""
 	else:
 		$StatusMessage.text ="Card scanned is not valid"
 
 func _scan_pressed() -> void:
+	if websocket.get_ready_state() != WebSocketPeer.STATE_OPEN:
+		start_reconnection()
 	send_character_switch_request()
+	get_username()
+
+
+	
