bb66c5c66accf236f11667536dcd5c86f0034cff
Nassim Madrane <nassim.madrane@proton.me>
2025-05-28 09:01:23 +0200

    fixed what i removed from global

--- Godot/integration-project-gamr/scripts/Global.gd
+++ Godot/integration-project-gamr/scripts/Global.gd
@@ -4,3 +4,11 @@ var username = "player"
 var character = "magician"
 var loggedin = false
 var victor = ""
+
+var current_player: Node2D = null
+
+func set_player(player: Node2D) -> void:
+	current_player = player
+
+func get_player() -> Node2D:
+	return current_player
